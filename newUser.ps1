049e1624c9b0d9f07f8b0e68b14a295d99638518
Jevons Kuang <jevonsk@pornhub.gov>
2019-08-19 13:23:30 -0400

    first commit

---
+++ newUser.ps1
@@ -0,0 +1,16 @@
+﻿Import-Csv -Path C:\users.csv | foreach {
+	#initial default password
+	$defaultPw = 'P@ssw0rd'
+	$secPw = ConvertTo-SecureString -String $defaultPw -AsPlainText -Force
+
+	$userName = '{0}{1}' -f $_.FirstName.Substring(0,1),$_.LastName
+	$userName.ToLower()
+	$NewUserParameters = @{
+		GivenName = $_.FirstName
+		Surname = $_.LastName
+		Name = $userName
+		AccountPassword = $secPw
+	}
+	New-AdUser @NewUserParameters
+	Add-AdGroupMember -Identity 'Students' -Members $userName
+}
